671e4c9b5c0907fc7942170d9046c72d3cffa331
孙楷 <704725181@qq.com>
2020-08-27 18:52:44 +1000

    Initial Commit

---
+++ FIT5140-assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FIT5140-assignment1
+//
+//  Created by sunkai on 27/8/20.
+//  Copyright © 2020 sunkai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
